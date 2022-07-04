fc0a7efcb9715952d97a3852eae5e9f8e98bc740
leooel88 <leofargis.fargis@gmail.com>
2022-07-04 15:53:41 +0200

    [ADD] ficheposte creation missing css/style

--- exp_must_azure_test/routes/FichePoste.js
+++ exp_must_azure_test/routes/FichePoste.js
@@ -4,10 +4,16 @@ const router = require('express-promise-router')();
 const fichePoste = require('../controller/FichePoste.js');
 
 router.get('/', function (req, res, next) {
+	let { error } = req.query;
+
 	let params = {
 		active: { fichePoste: true },
+		currentDate: new Date().toISOString().split('T')[0],
 	};
-	res.render('fichePoste');
+	if (error != null && error.length > 0) {
+		params.error = [{ message: error }];
+	}
+	res.render('fichePoste', params);
 });
 
 router.post('/create', fichePoste.create);
