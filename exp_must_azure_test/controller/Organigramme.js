597da0af2f359f2203f6c1b6bae6de6078ffb22a
ouergh_a <ouergh_a@etna-alternance.net>
2022-08-03 23:45:23 +0200

    fixed bugs with directReports + added organigramme front

--- exp_must_azure_test/controller/Organigramme.js
+++ exp_must_azure_test/controller/Organigramme.js
@@ -10,14 +10,21 @@ const manager = await azureService.getManager(
     req.session.userId  
 );
 
+const user = await azureService.getUserDetails(
+    req.app.locals.msalClient,
+    req.session.userId  
+);
+
 const directReports = await azureService.getDirectReports(
     req.app.locals.msalClient,
     req.session.userId  
 );
 
 params.manager = manager;
+params.user = user;
 params.directReports = directReports;
-console.log(params.directReports);
-res.render('organigramme',{manager:directReports,directReports:directReports});
+console.log(directReports.value);
+//console.log(manager);
+res.render('organigramme',{manager:manager,user:user,directReports:directReports.value[0]});
 
 }
