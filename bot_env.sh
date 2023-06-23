b5fd934395562e453cd938bd3a9e38ddc0ebd059
ngo_a <ngo_a@etna-alternance.net>
2023-06-23 09:08:03 +0200

    Update bot_env.sh

--- bot_env.sh
+++ bot_env.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-echo export OPENAI_API_KEY=$OPENAI_API_KEY >> ~/.profile
-echo export PINECONE_API_KEY=$PINECONE_API_KEY >> ~/.profile
-echo export PINECONE_ENVIRONMENT=$PINECONE_ENVIRONMENT >> ~/.profile
-echo export PINECONE_INDEX_NAME=$PINECONE_INDEX_NAME >> ~/.profile
+
+sed -i '/OPENAI_API_KEY/d' ~/.bashrc
+sed -i '/PINECONE_API_KEY/d' ~/.bashrc
+sed -i '/PINECONE_ENVIRONMENT/d' ~/.bashrc
+sed -i '/PINECONE_INDEX_NAME/d' ~/.bashrc
+echo export OPENAI_API_KEY=$OPENAI_API_KEY >> ~/.bashrc
+echo export PINECONE_API_KEY=$PINECONE_API_KEY >> ~/.bashrc
+echo export PINECONE_ENVIRONMENT=$PINECONE_ENVIRONMENT >> ~/.bashrc
+echo export PINECONE_INDEX_NAME=$PINECONE_INDEX_NAME >> ~/.bashrc
