6c7f26f37700f2170bb3af3bb5e7f5845491011e
achoui <achoui_h@etna-alternance.net>
2023-06-29 15:28:46 +0200

    envoie de mail

--- Eashire/modules/dossierRecrutement/controllers/sendMail.js
+++ Eashire/modules/dossierRecrutement/controllers/sendMail.js
@@ -1,4 +1,4 @@
-const { Candidature, DossierRecrutement, FichierRecrutement } = require('@models')
+const { Candidature, DossierRecrutement, FichierRecrutement, FichePoste } = require('@models')
 const mysql = require('mysql2')
 const nodemailer = require('nodemailer')
 
@@ -16,13 +16,36 @@ exports.process = async (req, res) => {
 		where: { id: candidatureId },
 	})
 
-	// const { dataValues: fichiersRecrutementData } = await FichierRecrutement.findAll({
-	// 	where: { dossierRecrutementId: dossierRecrutementId },
-	// })
+	const fichiersRecrutementData = await FichierRecrutement.findAll({
+		where: { dossierRecrutementId: dossierRecrutementId },
+	})
+
+	const fichiersRecrutementFormattedData = fichiersRecrutementData.map((fichierRecrutement) => {
+		return {
+			title: fichierRecrutement.dataValues.title,
+			description: fichierRecrutement.dataValues.description,
+		}
+	})
+
+	const { fichePosteId } = data[0]
+	const fichePoste = await FichePoste.findOne({
+		where: { id: fichePosteId },
+	})
+
+	const jobLabel = fichePoste.label
 
 	console.log(dossierRecrutementData)
 	console.log('====================')
 	console.log(data[0].mail)
+	console.log('====================')
+
+	console.log(fichiersRecrutementData)
+	console.log('====================')
+
+	console.log(fichiersRecrutementFormattedData)
+	console.log('====================')
+
+	console.log(jobLabel)
 	console.log('====================')
 
 	const transporter = nodemailer.createTransport({
@@ -32,13 +55,21 @@ exports.process = async (req, res) => {
 			pass: 'wofgpkqnlrmtsfrs',
 		},
 	})
+	let mailtext =
+		`Bonjour,\n\n` +
+		`J ai le plaisir de vous annoncer que votre candidature au poste de ${jobLabel} à été retenue .\n Afin de mener à bien votre intégration administrative, nous avons besoin de certains documents que vous devrez nous \n`
+	// let mailtext =
+	// 	'Bonjour,\n\n' +
+	// 	"Je suis ravi de vous accueillir en tant que nouveau candidat.\n Nous sommes impatients de découvrir vos talents et compétences, et de voir comment vous pouvez contribuer à notre équipe et à notre organisation.\n Nous espérons que votre expérience ici sera enrichissante et passionnante, et que vous trouverez un environnement de travail stimulant et collaboratif. N'hésitez pas à poser des questions et à faire part de vos idées et suggestions. \n Nous sommes là pour vous aider à réussir et à atteindre vos objectifs professionnels.\n\n Bonne chance et bienvenue à bord !"
+	for (const fichier of fichiersRecrutementFormattedData) {
+		mailtext += ` - ${fichier.title} : ${fichier.description} \n`
+	}
+
 	const mailOptions = {
 		from: 'achoui.hillal@gmail.com',
 		to: data[0].mail,
 		subject: "Offre d'emploi ",
-		text:
-			'Bonjour,\n\n' +
-			"Je suis ravi de vous accueillir en tant que nouveau candidat.\n Nous sommes impatients de découvrir vos talents et compétences, et de voir comment vous pouvez contribuer à notre équipe et à notre organisation.\n Nous espérons que votre expérience ici sera enrichissante et passionnante, et que vous trouverez un environnement de travail stimulant et collaboratif. N'hésitez pas à poser des questions et à faire part de vos idées et suggestions. \n Nous sommes là pour vous aider à réussir et à atteindre vos objectifs professionnels.\n\n Bonne chance et bienvenue à bord !",
+		text: mailtext,
 	}
 	transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
